e6b03d6393d02bc74087ec24e03a619b331577bb
Uttam Kumar <uk89197@gmail.com>
2025-06-16 09:36:49 +0530

    Delete BridgeLab Java directory

--- BridgeLab Java/Day 1/TwoSum.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class TwoSum{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        System.out.println("the Sum of both nos is " + (a+b));
-    }
-}
